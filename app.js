5ef804b06c66d44594db3ccf0d7ddb307455f475
Ummar Farooq <farooqcherur@gmail.com>
2022-12-10 09:25:59 +0530

    final setup

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ const hbs=require('express-handlebars')
 const HBS=hbs.create({})
 var db=require('./config/connection')
 var session=require('express-session')
-//its used to store session in hosting db
-const { CyclicSessionStore } = require("@cyclic.sh/session-store");
-
 var Handlebars=require('handlebars')
 
 var userRouter = require('./routes/user');
@@ -19,7 +16,6 @@ var adminRouter = require('./routes/admin');
 var app = express();
 var fileUpload=require('express-fileupload');
 const { dirname } = require('path');
-
 
 
 
@@ -61,32 +57,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload())
-
-//its used to store session in hosting db
-const options = {
-  table: {
-    name: process.env.CYCLIC_DB,
-  },
-  keepExpired: false,
-  touchInterval: 30000, // milliseconds (30 seconds)
-  ttl: 86400000 // milliseconds (1 day)
-};
-
-
-app.set('trust-proxy', 1)
 app.use(session({
-  store: new CyclicSessionStore(options),
-  secret: 'key',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: 'auto', // (process.env.NODE_ENV != 'development'),
-    maxAge: 2000000
-  }
-  // unset: "destroy"
+  secret:'key',
+  resave:false,
+  cookie:{maxAge:2000000},
+  saveUninitialized:false
+
 }))
 
-
+db.connect((err)=>{
+  if(err) console.log('connection error'+err);
+  else console.log('database connected');
+})
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
 
@@ -112,13 +94,5 @@ app.use(function (err, req, res, next) {
 });
 
 
-db.connect((err)=>{
-  if(err) console.log('connection error'+err);
-  else console.log('database connected');
-  app.listen(process.env.PORT,()=>{
-    
-    console.log('listening for request');
-  })
-})
 
 module.exports = app;
